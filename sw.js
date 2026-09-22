28295f6cdbb1d43da26bf1a5a593f498acc2faa5
giostacchio <31801153+giostacchio@users.noreply.github.com>
2026-09-22 08:54:12 +0200

    Aggiorna cache HUB per nuove risposte CNA Lampo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='giostacchio-hub-v4';
+const CACHE='giostacchio-hub-v5';
 const CORE=['./','./index.html','./cna-lampo.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',e=>{
